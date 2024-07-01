0d379ba57e51fe718531af2fa07fc7d0911418c7
Naitik Patel <naitikpatel.np2004@gmail.com>
2024-07-01 18:52:11 +0530

    first commit

---
+++ src/main/java/com/HMSApp/Hospital/Management/System/doclogin/entity/Medicine.java
@@ -0,0 +1,50 @@
+package com.HMSApp.Hospital.Management.System.doclogin.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="medicine")
+public class Medicine {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="drug_name")
+	private String drugname;
+	private String stock;
+	public Medicine(long id, String drugname, String stock) {
+		super();
+		this.id = id;
+		this.drugname = drugname;
+		this.stock = stock;
+	}
+	public Medicine() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDrugname() {
+		return drugname;
+	}
+	public void setDrugname(String drugname) {
+		this.drugname = drugname;
+	}
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	
+	
+}
